9c349e47a8b09c7be2ae581b7491701498503aa4
Smooth <92906509+VladimirGladky@users.noreply.github.com>
2022-12-02 02:21:02 +0300

    улучшил задания на день

--- lib/tasks_list/tasks_list.dart
+++ lib/tasks_list/tasks_list.dart
@@ -13,6 +13,7 @@ class TasksListWidget extends StatelessWidget {
       'Ужин',
       'Сон'
     ];
+    var col = Colors.green;
 
     return ListView.separated(
       padding: const EdgeInsets.all(8),
@@ -20,14 +21,26 @@ class TasksListWidget extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         return Container(
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(10)),color: Colors.green),
+              borderRadius: BorderRadius.all(Radius.circular(10)), color: col),
           height: 50,
-          child: Center(
+          child: Row(
+            children: [
+              ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.incomplete_circle_sharp,
+                    color: Colors.red,
+                  ),
+                  label: Text('')),
+              Padding(padding: EdgeInsets.fromLTRB(80,0, 50, 0),
               child: Text(
-            '${entries[index]}',
-            style: TextStyle(fontSize: 20),
-          )),
+                '${entries[index]}',
+                style: TextStyle(fontSize: 25),
+              )),
+            ],
+          ),
         );
+        //child: Center(child: Text('Entry ${entries[index]}'))
       },
       separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
