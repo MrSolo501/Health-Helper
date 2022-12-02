503eb25ba9a63a8fa8dfb07a929236b994a353f8
MrSolo501 <mikhinmikhail501@gmail.com>
2022-12-02 19:30:59 +0300

    Обновил страницу настроек

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -5,8 +5,61 @@ class SettingsScreenWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 15),
+          child: TextFormField(
+            style: const TextStyle(color: Color.fromRGBO(56, 102, 65, 1)),
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(
+                      width: 2.0, color: Color.fromRGBO(106, 153, 78, 1))),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(
+                      width: 3.0, color: Color.fromRGBO(167, 201, 87, 1))),
+              labelText: 'Введите ваш рост',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: TextFormField(
+            style: const TextStyle(color: Color.fromRGBO(56, 102, 65, 1)),
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(
+                      width: 2.0, color: Color.fromRGBO(106, 153, 78, 1))),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(
+                      width: 3.0, color: Color.fromRGBO(167, 201, 87, 1))),
+              labelText: 'Введите ваш вес',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 15, 20, 0),
+          child: TextFormField(
+            style: const TextStyle(color: Color.fromRGBO(56, 102, 65, 1)),
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(
+                      width: 2.0, color: Color.fromRGBO(106, 153, 78, 1))),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(
+                      width: 3.0, color: Color.fromRGBO(167, 201, 87, 1))),
+              labelText: 'Введите ваш возраст',
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
