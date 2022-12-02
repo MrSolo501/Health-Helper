bbdb2b71f8a503b6cb436596f09d55fdd79cce41
Smooth <92906509+VladimirGladky@users.noreply.github.com>
2022-12-02 22:00:29 +0300

    добавил в рацион вкладки завтрак обед ужин и

--- lib/pages/eda.dart
+++ lib/pages/eda.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:health_helper/pages/breakfast_page.dart';
+import 'breakfast_page.dart';
+import 'lunch_page.dart';
+import 'dinner_page.dart';
+
+final List<String> entries = <String>['Завтрак', 'Обед', 'Ужин'];
+final List<int> colorCodes = <int>[600, 600, 600];
 
 class EdaScreenWidget extends StatelessWidget {
-  const EdaScreenWidget({Key? key}) : super(key: key);
+  const EdaScreenWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => breakfastScreen()));
+            },
+            child: Container(
+              margin: const EdgeInsets.all(10.0),
+              height: 100,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.lightGreen),
+              child: Center(
+                  child: Text(
+                'Завтрак',
+                style: TextStyle(fontSize: 40),
+              )),
+            )),
+        GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => lunchScreen()));
+            },
+            child: Container(
+          margin: const EdgeInsets.all(10.0),
+          height: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.lightGreen),
+          child: Center(
+              child: Text(
+            'Обед',
+            style: TextStyle(fontSize: 40),
+          )),
+        )),
+        GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => dinnerScreen()));
+            },
+            child: Container(
+          margin: const EdgeInsets.all(10.0),
+          height: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.lightGreen),
+          child: Center(
+              child: Text(
+            'Ужин',
+            style: TextStyle(fontSize: 40),
+          )),
+        )),
+      ],
     );
   }
 }
--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'home_page.dart';
 
 void main() {
@@ -6,8 +7,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       title: 'Health Helper',
       theme: ThemeData(
